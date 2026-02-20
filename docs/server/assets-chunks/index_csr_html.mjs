607e1e208e005132f6e9f692302356ce91026694
Jose Mijangos <josemijangos@Joses-MacBook-Pro.local>
2026-02-19 20:26:11 -0800

    Initial commit

--- docs/server/assets-chunks/index_csr_html.mjs
+++ docs/server/assets-chunks/index_csr_html.mjs
@@ -6,8 +6,8 @@ export default `<!doctype html>
   <base href="/angular-app/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
-<link rel="stylesheet" href="styles-5INURTSO.css"></head>
+<link rel="stylesheet" href="https://josethedatanerd.github.io/online/styles-5INURTSO.css"></head>
 <body ngcm="">
   <app-root></app-root>
-<script src="main-SCUKGYXG.js" type="module"></script></body>
+<script src="https://josethedatanerd.github.io/online/main-SCUKGYXG.js" type="module"></script></body>
 </html>`;
