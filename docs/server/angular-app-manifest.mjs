c5b3021483fc86abfe5ea0d7cccdb2c5005492a6
Jose Mijangos <josemijangos@Joses-MacBook-Pro.local>
2026-02-19 20:39:09 -0800

    Initial commit

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 544, hash: '180baa0fe7d3596da4fd7deab896c9757911140a81cde3fca83ef836b3a810d0', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1057, hash: 'c6a6ed3591d87d474903cd602edf28d55645b14a71a63fb3ee7e69dc96130b87', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 16494, hash: '352128e464277ab4355eacf8945c37f73f604cdbf6c70b5785107eaf197aff62', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 462, hash: '5c4d0b1616d5f4d4360b817513d3a9ec228041c8ee3769c2b25ea11ddc60cb50', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 975, hash: '452603674295a2167b4e49cac21c5967143f681de0a0c6d939a187331e6624d3', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 16412, hash: 'cad705954ddbf18bdd0f0f7aee20cc7cdda8937a604b01787db792d2bffd8485', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
